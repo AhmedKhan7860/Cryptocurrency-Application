468d08a7cf1776f168bb873fd80606f368a7cc7d
Anna <a.m.kapourani@se23.qmul.ac.uk>
2025-03-24 13:41:34 +0000

    signin and signup auth

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,23 +1,73 @@
-import { View, Text, TouchableOpacity, Image, TextInput } from "react-native";
+import { View, Text, TouchableOpacity, Image, TextInput, Alert } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React, { useState } from "react";
-import { useNavigation } from "@react-navigation/native";
-
+import { useSignUp } from "@clerk/clerk-expo";
+import { useRouter } from "expo-router";
 
 export default function SignupScreen() {
     const [firstname, setFirstName] = useState("");
     const [lastname, setLastName] = useState("");
-    const [username, setUsernameName] = useState("");
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [showPassword, setShowPassword] = useState(false);
-    const [email, setEmail] = useState("");
+    const [emailAddress, setEmailAddress] = useState("");
+    const router = useRouter();
 
-    const navigation = useNavigation();
+    
+    const { signUp, isLoaded, setActive } = useSignUp();
+    const [loading, setLoading] = useState(false);
+
+ 
+    // create user and send the verification email
+    const SignupPress = async () => {
+        if(!isLoaded){
+            Alert.alert("Error", "Clerk is not loaded.")
+            return;
+        }
+        setLoading(true);
+
+        try{
+            if (!emailAddress){
+                Alert.alert("Error", "Email address is required");
+                return;
+            }
+            // create user on clerk
+            const result = await signUp.create({
+                emailAddress,
+                password,
+                firstName: firstname,
+                lastName: lastname,
+                username: username,
+            });
+
+            console.log("Sign-up response:", result);
+
+            if (result.status === 'complete'){
+                await setActive({ session: result.createdSessionId});
+                router.replace('/(tabs)/home');
+            }else if (result.status === "missing_requirements") {
+                
+                // Prepare email verification
+                await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+    
+                // Navigate to the verification screen
+                router.replace('/verification');
+            }
+            else{
+                Alert.alert("Error", "Sign Up process is incomplete")
+            }
+        }catch(error: any){
+            Alert.alert("Error", error.errors[0].message);
+        }finally{
+            setLoading(false);
+        }
+    }
 
     return(
         <SafeAreaView className="bg-black p-4">
-            <View className="p-6 bg-black">
+
+            <View className="p-6 bg-black mb-0 -mt-12">
 
                 {/* Header */}
                 <View className="mb-17 items-center">
@@ -27,7 +77,7 @@ export default function SignupScreen() {
 
                 {/*  Social Logup Buttons  */}
                 <View className="flex-row items-center gap-7 mb-7">
-                    <TouchableOpacity className="flex-row items-center justify-center w-[150px] h-[35px] rounded-md mb-4 bg-[rgb(52,52,52)]">
+                    <TouchableOpacity className="flex-row items-center justify-center w-[150px] h-[35px] rounded-md mb-4 bg-[rgb(52,52,52)]" onPress={handleGoogleSignUp}>
                         <Image
                             source={{ uri: "https://img.icons8.com/?size=100&id=17950&format=png&color=ffffff" }}
                             className="w-5 h-5 mr-3"
@@ -95,7 +145,7 @@ export default function SignupScreen() {
                                 className="flex-1 text-white"
                                 placeholderTextColor="#666"
                                 value={username}
-                                onChangeText={setUsernameName}
+                                onChangeText={setUsername}
                                 keyboardType="default"
                                 autoCapitalize="words"
                             />
@@ -109,10 +159,10 @@ export default function SignupScreen() {
                             <TextInput
                                 className="flex-1 text-white"
                                 placeholderTextColor="#666"
-                                value={email}
-                                onChangeText={setEmail}
+                                value={emailAddress}
+                                onChangeText={setEmailAddress}
                                 keyboardType="default"
-                                autoCapitalize="words"
+                                autoCapitalize="none"
                             />
                         </View>
                 </View>
@@ -139,22 +189,28 @@ export default function SignupScreen() {
                 </View>
 
                 {/* Sign Up Button */}
-                <TouchableOpacity className="bg-purple-300 rounded-full h-14 justify-center items-center mb-5 -mt-2">
+                <TouchableOpacity className="bg-purple-300 rounded-full h-14 justify-center items-center mb-5 -mt-2" onPress={SignupPress} disabled={loading}>
                     <Text className="text-base font-bold text-background">
-                        Sign Up
+                        {loading ? "Loading..." : "Sign Up"}
                     </Text>
                 </TouchableOpacity>
+            
 
                 {/* Message */}
                 <View className="items-center">
-                    <TouchableOpacity onPress={() => navigation.navigate('signin')}>
+                    <TouchableOpacity onPress={() => router.replace('/(public)/sign-in')}>
                         <Text className="text-white">
                             Already have an account? <Text className="text-purple-300 font-bold">Sign in</Text>
                         </Text>
                     </TouchableOpacity>
                 </View>
+
             </View>
+
         </SafeAreaView>
 
+
+
     )
+
 }
